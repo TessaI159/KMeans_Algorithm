04af289c268af7f4d1ca69903988f43c1cec20ac
Tess Garrison <tessag159@icloud.com>
2021-03-03 20:45:01 -0600

    Runs fine until frame 52

--- src/videoProcess.cpp
+++ src/videoProcess.cpp
@@ -6,6 +6,7 @@
 
 #include <opencv2/opencv.hpp>
 
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -16,18 +17,27 @@ void processVideo(std::string filename, int centroids, double ratio)
 
   int index{0};
   int frames(video.get(cv::CAP_PROP_FRAME_COUNT));
-  
+
   while(true)
     {
+      // clock_t frameCreationStart{clock()};
       cv::Mat frame;
       video >> frame;
-
+      // clock_t frameCreationEnd{clock()};
+      // std::cout << "Frame creation: " <<  (static_cast<double>(frameCreationEnd
+      // 							   - frameCreationStart) /
+      // 				       static_cast<double>(CLOCKS_PER_SEC)) << "\n";
       if(frame.empty())
 	{
 	  break;
 	}
       std::cout << ++index << "/" << frames << "\n";
+      clock_t start{clock()};
       processFrame(frame, centroids, ratio);
+      clock_t end{clock()};
+
+      std::cout << "Frame time: " <<  (static_cast<double>(end - start) /
+				       static_cast<double>(CLOCKS_PER_SEC)) << "\n";
     }
   video.release();
 }
@@ -46,8 +56,13 @@ void processFrame(cv::Mat frame, int centroids, double ratio)
 
   std::vector<Pixel> pixelVector{scanImage(resizedFrame)};
   std::vector<Centroid> centroidVector{createCentroids(pixelVector, centroids)};
-  
+
+  clock_t updateStart{clock()};
   while(updateCentroids(centroidVector, pixelVector)){}
-  
-  
+  clock_t updateEnd{clock()};
+
+  // std::cout << "Centroid update: " <<  (static_cast<double>(updateEnd - updateStart) /
+  //            static_cast<double>(CLOCKS_PER_SEC)) << "\n";
+
+
 }
