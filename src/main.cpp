c491149ae082aa2ecb44dfe92f8c3d94d4fb8ff4
Tess Garrison <tessag159@icloud.com>
2021-03-06 10:51:49 -0600

    Updated todos, branching.

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,15 @@
 #include <opencv2/opencv.hpp>
 
 // TODO
-// Intelligent auto number of centroids using elbow method
+// Intelligent auto number of centroids using elbow method - COMPLETED
 // Auto optimal resize - COMPLETED
 // Automatic conversion of video into compatible format/codec with ffmpeg library
 // Implement unique pointers to ensure each pixel in the vector only has one pointer to it
 // Figure out default variables for a forward-declared function
 // Class constructor initialization lists
-// New, delete, smart pointers
-// Trim any pure blck frames off the beginning of the video
+// Smart pointers
+// Trim any pure blck frames off the beginning and end of the video
 
-
-// Distortion: It is calculated as the average of the squared distances from the
-// cluster centers of the respective clusters. Typically, the Euclidean distance metric is used.
-
-// For the elbow test use distortion, current point calculation, and 2..10 centroids inclusive
 
 
 int main()
