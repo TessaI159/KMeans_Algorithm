34c67415339c76a205d19447275d24ff43603075
Tess Garrison <tessag159@icloud.com>
2021-03-07 10:05:52 -0600

    Started to test color spaces

--- src/constants.h
+++ src/constants.h
@@ -2,7 +2,7 @@
 #define CONSTANTS_H
 
 constexpr int MAX_CENTROIDS{6};
-constexpr int TARGET_PIXELS{800};
-constexpr int MIN_PIXELS{0};
+constexpr int TARGET_PIXELS{1800};
+constexpr int MIN_PIXELS{800};
 
 #endif
