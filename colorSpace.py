1d7b0082dba35a21047b743e912103b6135840b3
Tess Garrison <tessag159@icloud.com>
2021-03-08 21:23:36 -0600

    Finished writing difference tests for all formula I could find.

--- colorSpace.py
+++ colorSpace.py
@@ -20,13 +20,13 @@ class sRGB:
         self.sR = sR
         self.sG = sG
         self.sB = sB
-    
-    
+
+
 class XYZ:
     x = 0.0
     y = 0.0
     z = 0.0
-    
+
     def __init__(self):
         self.x = 0.0
         self.y = 0.0
@@ -43,6 +43,7 @@ class Lab:
         self.a = 0.0
         self.b = 0.0
 
+
 class Lch:
     l = 0.0
     c = 0.0
@@ -52,6 +53,7 @@ class Lch:
         self.l = 0.0
         self.c = 0.0
         self.h = 0.0
+
 
 class Color:
     def __init__(self, r, g, b):
@@ -65,24 +67,25 @@ class Color:
         self.__sRGBtoXYZConversion()
         self.__XYZtoCIELabConversion()
         self.__CIELabtoCIELchConversion()
-        
+
+
     def __sRGBtoXYZConversion(self):
         # X Y and Z use a standard  D65/2° illuminant
-        
+
         self.rgb._vR = self.rgb.sR / 255.0
         self.rgb._vG = self.rgb.sG / 255.0
         self.rgb._vB = self.rgb.sB / 255.0
-        
+
         if(self.rgb._vR > 0.04045):
             self.rgb._vR = ((self.rgb._vR + 0.055) / 1.055)**2.4
         else:
             self.rgb._vR /= 12.92
-            
+
         if(self.rgb._vG > 0.04045):
             self.rgb._vG = ((self.rgb._vG + 0.055) / 1.055)**2.4
         else:
-            self.rgb._vG /= 12.92        
-        
+            self.rgb._vG /= 12.92
+
         if(self.rgb._vB > 0.04045):
             self.rgb._vB = ((self.rgb._vB + 0.055) / 1.055)**2.4
         else:
@@ -96,10 +99,10 @@ class Color:
         self.xyz.y = (self.rgb._vR * 0.2126) + (self.rgb._vG * 0.7152) + (self.rgb._vB * 0.0722)
         self.xyz.z = (self.rgb._vR * 0.0193) + (self.rgb._vG * 0.1192) + (self.rgb._vB * 0.9505)
 
-        
+
     def __XYZtoCIELabConversion(self):
         # _X_2, _Y_2, and _Z_2 use a D65/2° standard illuminant
-        
+
         self.xyz._vX = self.xyz.x / self._X_2
         self.xyz._vY = self.xyz.y / self._Y_2
         self.xyz._vZ = self.xyz.z / self._Z_2
@@ -108,12 +111,12 @@ class Color:
             self.xyz._vX = self.xyz._vX**(1.0 / 3.0)
         else:
             self.xyz._vX = (7.787 * self.xyz._vX) + (16.0 / 116.0)
-            
+
         if(self.xyz._vY > 0.008856):
             self.xyz._vY = self.xyz._vY**(1.0 / 3.0)
         else:
             self.xyz._vY = (7.787 * self.xyz._vY) + (16.0 / 116.0)
-            
+
         if(self.xyz._vZ > 0.008856):
             self.xyz._vZ = self.xyz._vZ**(1.0 / 3.0)
         else:
@@ -122,6 +125,7 @@ class Color:
         self.lab.l = (116.0 * self.xyz._vY) - 16
         self.lab.a = 500.0 * (self.xyz._vX - self.xyz._vY)
         self.lab.b = 200.0 * (self.xyz._vY - self.xyz._vZ)
+
 
     def __CIELabtoCIELchConversion(self):
         self.lch._vH = math.atan2(self.lab.b, self.lab.a)
@@ -136,11 +140,11 @@ class Color:
         self.lch.h = self.lch._vH
 
 
-def simpleEuclideanRGBDistance(color1, color2):
+def simpleEuclideanDifference(color1, color2):
     return math.sqrt(((color1.rgb.sR - color2.rgb.sR)**2) + ((color1.rgb.sG - color2.rgb.sG)**2) + ((color1.rgb.sB - color2.rgb.sB)**2))
 
 
-def weightedEuclideanRGBDistance(color1, color2):
+def weightedEuclideanDifference(color1, color2):
     if(((color1.rgb.sR + color2.rgb.sR) / 2) < 128):
         r = 2.0
         b = 3.0
@@ -150,29 +154,29 @@ def weightedEuclideanRGBDistance(color1, color2):
     return math.sqrt(((r * (color1.rgb.sR - color2.rgb.sR)**2) + (4.0 * (color1.rgb.sG - color2.rgb.sR)**2) + (b * (color1.rgb.sB - color2.rgb.sB)**2)))
 
 
-def redmeanEuclideanRGBDistance(color1, color2):
+def redmeanEuclideanDifference(color1, color2):
     rAverage = ((color1.rgb.sR + color2.rgb.sR) / 2)
 
     return math.sqrt(((2 + (rAverage / 256) * (color1.rgb.sR - color2.rgb.sR)**2) + ((4 * (color1.rgb.sG - color2.rgb.sG)**2)) + ((2 + ((255 - rAverage) / 256)) * (color1.rgb.sB - color2.rgb.sB)**2)))
 
 
-def taxicabLABDistance(color1, color2):
+def taxicabDifference(color1, color2):
     return math.sqrt(((color1.lab.a - color2.lab.a)**2) + ((color1.lab.b - color2.lab.b)**2)) + abs(color1.lab.l - color2.lab.l)
 
 
-def deltaCDistance(color1, color2):
+def deltaCDifference(color1, color2):
     return math.sqrt((color2.lab.a**2) + (color2.lab.b**2)) - math.sqrt((color1.lab.a**2) + (color1.lab.b**2))
 
 
-def deltaHDistance(color1, color2):
-    return math.sqrt(((color2.lab.a - color2.lab.b)**2) + ((color1.lab.b - color1.lab.a)**2) - ((deltaCDistance(color1, color2))**2))
+def deltaHDifference(color1, color2):
+    return math.sqrt(((color2.lab.a - color2.lab.b)**2) + ((color1.lab.b - color1.lab.a)**2) - ((deltaCDifference(color1, color2))**2))
 
+# Tested: Working
+def deltaE76Difference(color1, color2):
+    return math.sqrt(((color1.lab.l - color2.lab.l)**2) + ((color1.lab.a - color2.lab.a)**2) + ((color1.lab.b - color2.lab.b)**2))
 
-def deltaECIEDistance(color1, color2):
-    return math.sqrt(((color1.lab.l - color2.lab.l)**2) + ((color1.lab.l - color2.lab.a)**2) + ((color1.lab.b - color2.lab.b)**2))
-
-
-def deltaE94Distance(color1, color2):
+# Tested: Working
+def deltaE94Difference(color1, color2):
     whtL = 1
     whtC = 1
     whtH = 1
@@ -180,13 +184,13 @@ def deltaE94Distance(color1, color2):
     xC2 = math.sqrt((color2.lab.a**2) + (color2.lab.b**2))
     xDL = color2.lab.l - color1.lab.l
     xDC = xC2 - xC1
-    xDE = deltaECIEDistance(color1, color2)
+    xDE = deltaE76Difference(color1, color2)
     xDH = (xDE**2) - (xDL**2) - (xDC**2)
 
     if(xDH > 0):
         xDH = math.sqrt(xDH)
     else:
-        xDH = 0        
+        xDH = 0
 
     xSC = 1 + (0.045 * xC1)
     xSH = 1 + (0.015 * xC1)
@@ -196,77 +200,113 @@ def deltaE94Distance(color1, color2):
 
     return math.sqrt((xDL**2) + (xDC**2) + (xDH**2))
 
-def LabtoHue(a, b):
-    if(a >= 0 and b == 0):
-        return 0
-    elif(a < 0 and b == 0):
-        return 180
-    elif(a == 0 and b > 0):
-        return 90
-    elif(a == 0 and b < 0):
-        return 270
-    elif(a > 0 and b > 0):
-        bias = 0
-    elif(a < 0):
-        bias = 180
-    elif(a > 0 and b < 0):
-        bias = 360
+# Untested
+def deltaE00Difference(color1, color2):
+    K_L = K_C = K_H = 1.0
+    deltaLTick = color2.lab.l - color1.lab.l
+    LBarTick = (color1.lab.l + color2.lab.l) / 2
+    CBar = (color1.lch.c + color2.lch.c) / 2
+    aTick_1 = color1.lab.a + (color1.lab.a / 2) * (1 - math.sqrt(CBar**7 / (CBar**7 + 25**7)))
+    aTick_2 = color2.lab.a + (color2.lab.a / 2) * (1 - math.sqrt(CBar**7 / (CBar**7 + 25**7)))
 
-    return (math.degrees(math.atan(b / a)) + bias)
+    CTick_1 = math.sqrt(aTick_1**2 + color1.lab.b**2)
+    CTick_2 = math.sqrt(aTick_2**2 + color2.lab.b**2)
+    CBarTick = (CTick_1 + CTick_2) / 2
+    deltaCTick = CTick_2 - CTick_1
 
-
-def deltaE00Distance(color1, color2):
-    whtL = 1
-    whtC = 1
-    whtH = 1
-
-    xC1 = math.sqrt((color1.lab.a**2) + (color1.lab.b**2))
-    xC2 = math.sqrt((color2.lab.a**2) + (color2.lab.b**2))
-    xCX = (xC1 + xC2) / 2
-    xGX = 0.5 * (1 - math.sqrt((xCX**7) / ((xCX**7) + (25**7))))
-    xNN = (1 + xGX) * color1.lab.a
-    xC1 = math.sqrt((xNN**2) + (color1.lab.b**2))
-    xH1 = LabtoHue(xNN, color1.lab.b)
-    xNN = (1 + xGX) * color2.lab.a
-    xC2 = math.sqrt((xNN**2) + (color2.lab.b**2))
-    xH2 = LabtoHue(xNN, color2.lab.b)
-    xDL = color2.lab.l - color1.lab.l
-    xDC = xC2 - xC1
-
-    if((xC1 * xC2) == 0):
-        xDH = 0
+    if(aTick_1 == 0 and color1.lab.b == 0):
+        hTick_1 = 0
     else:
-        xNN = round(xH2 - xH1, 12)
-        if((abs(xNN)) <= 180):
-            xDH = xH2 - xH1
-        else:
-            if(xNN > 180):
-                xDH = xH2 - xH1 - 360
-            else:
-                xDH = xH2 - xH1 + 360
+        hTick_1 = math.atan2(color1.lab.b, aTick_1) % 360
 
-    xDH = 2 * math.sqrt(xC1 * xC2) * math.sin(math.radians(xDH / 2))
-    xLX = (color1.lab.l + color2.lab.l) / 2
-    xCY = (xC1 + xC2) / 2
-    if((xC1 * xC2) == 0):
-        xHX = xH1 + xH2
+    if(aTick_2 == 0 and color2.lab.b == 0):
+        hTick_2 = 0
     else:
-        xNN = abs(round((xH1 - xH2), 12))
-        if(xNN > 180):
-            if((xH2 + xH1) < 360):
-                xHX = xH1 + xH2 + 360
-            else:
-                xHX = xH1 + xH2 - 360
-        else:
-            xHX = xH1 + xH2
-        xHx /= 2
+        hTick_2 = math.atan2(color2.lab.b, aTick_2) % 360
 
-    # This line isn't even close to done
-    xTX = 1 - 0.17 * math.cos(math.radians(xHX - 30)) + 0.24 * math.cos()
+    if(CTick_1 == 0 or CTick_2 == 0):
+        deltahTick = 0
+    else:
+        if(abs(hTick_1 - hTick_2) <= 180):
+            deltahTick = hTick_2 - hTick_1
+        elif(abs(hTick_1 - hTick_2) > 180 and hTick_2 <= hTick_1):
+            deltahTick = hTick_2 - hTick_1 + 360
+        elif(abs(hTick_1 - hTick_2) > 180 and hTick_2 > hTick_1):
+            deltahTick = hTick_2 - hTick_1 - 360
 
-def deltaCMCDistance(color1, color2):
-    pass
+    deltaHTick = 2 * math.sqrt(CTick_1 * CTick_2) * math.sin(deltahTick / 2)
 
+    if(CTick_1 == 0 or CTick_2 == 0):
+        HBarTick = hTick_1 + hTick_2
+    else:
+        if(abs(hTick_1 - hTick_2) <= 180):
+            HBarTick = (hTick_1 + hTick_2) / 2
+        elif(abs(hTick_1 - hTick_2) > 180 and hTick_1 + hTick_2 < 360):
+            HBarTick = (hTick_1 + hTick_2 + 360) / 2
+        elif(abs(hTick_1 - hTick_2) > 180 and hTick_1 + hTick_2 >= 360):
+            HBarTick = (hTick_1 + hTick_2 - 360) / 2
+
+    T = 1 - 0.17 * math.cos(HBarTick - 30) + 0.24 * math.cos(2 * HBarTick) + 0.32 * math.cos(3 * HBarTick + 6) - 0.2 * math.cos(4 * HBarTick - 63)
+    S_L = 1 + ((0.015 * (LBarTick - 50)**2) / math.sqrt(20 + (LBarTick - 50)**2))
+    S_C = 1 + 0.045 * CBarTick
+    S_H = 1 + 0.015 * CBarTick * T
+    R_T = -2 * math.sqrt(CBarTick**7 / (CBarTick**7 + 25**7)) * math.sin(-(((HBarTick - 275) / 25)**2))
+
+    arg_L = deltaLTick / (K_L * S_L)
+    arg_C = deltaCTick / (K_C * S_C)
+    arg_H = deltaHTick / (K_H * S_H)
+
+    return math.sqrt(arg_L**2 + arg_C**2 + arg_H**2 + (R_T * arg_C * arg_H))
+
+
+# Untested
+def deltaCMCDifference(color1, color2, lightness=2, chroma=1):
+    if(color1.lch.l < 16):
+        S_L = 0.511
+    else:
+        S_L = ((0.040975 * color1.lch.l) / (1 + 0.01765 * color1.lch.l))
+
+    S_C = ((0.0638 * color1.lch.c) / (1 + 0.0131 * color1.lch.c)) + 0.638
+    F = math.sqrt((color1.lch.c**4) / (color1.lch.h**4 + 1900))
+    if(color1.lch.h >= 164 and color1.lch.h <= 345):
+        T = 0.56 + abs(0.2 * math.cos(color1.lch.h + 168))
+    else:
+        T = 0.36 + abs(0.4 * math.cos(color1.lch.h + 35))
+
+    S_H = S_C * (F * T + 1 - F)
+
+    arg_L = ((color2.lch.l - color1.lch.l) / (lightness * S_L))
+    arg_C = ((color2.lch.c - color1.lch.l) / (chroma * S_C))
+    arg_H = ((deltaHDifference(color1, color2) / (S_H)))
+
+    return math.sqrt((arg_L)**2 + (arg_C)**2 + (arg_H)**2)
+
+
+def allDifferences(ref, color):
+    print(f'Ref: ({ref.rgb.sR}, {ref.rgb.sG}, {ref.rgb.sB}) vs Color: ({color.rgb.sR}, {color.rgb.sG}, {color.rgb.sB})')
+    print("Simple Euclidean: ", end="")
+    print(simpleEuclideanDifference(ref, color))
+    print("Weighted Euclidean: ", end="")
+    print(weightedEuclideanDifference(ref, color))
+    print("Redmean Euclidean: ", end="")
+    print(redmeanEuclideanDifference(ref, color))
+    print("Taxicab: ", end="")
+    print(taxicabDifference(ref, color))
+    print("Delta C: ", end="")
+    print(deltaCDifference(ref, color))
+    print("Delta H: ", end="")
+    print(deltaHDifference(ref, color))
+    print("Delta E 1976: ", end="")
+    print(deltaE76Difference(ref, color))
+    print("Delta E 1994: ", end="")
+    print(deltaE94Difference(ref, color))
+    print("Delta E 2000: ", end="")
+    print(deltaE00Difference(ref, color))
+    print("Delta CMC: ", end="")
+    print(deltaCMCDifference(ref, color))
+    print()
+    print()
+    print()
 
 def createCentroidList(file):
     centroidList = []
@@ -276,7 +316,7 @@ def createCentroidList(file):
 
     targetPixels = lines.pop(-1)
     executionTime = lines.pop(-1)
-    
+
     for line in lines:
         if(len(line) == 0):
             lines.remove(line)
@@ -308,12 +348,9 @@ def createCentroidList(file):
     return (centroidList, targetPixels, executionTime)
 
 
-
-  
-
 def compareOutputs(centroidListSmall, centroidListLarge, executionSmall, executionLarge, pixelsSmall, pixelsLarge):
-    for x in range(len((centroidListSmall))):
-        pass
+    pass
+
 
 def largest(list):
     largestItem = list[0]
@@ -336,43 +373,54 @@ centroidListExecution = []
 ############# MAIN #############
 ################################
 
-color1 = Color(255, 193, 204)
-color2 = Color(225, 221, 172)
+# refColor = Color(178, 4, 154)
+# color1 = Color(44, 187, 51)
+# color2 = Color(181, 59, 86)
+# color3 = Color(252, 197, 23)
+# color4 = Color(126, 195, 77)
+# color5 = Color(195, 130, 200)
+# color6 = Color(212, 7, 115)
+# color7 = Color(82, 145, 36)
+# color8 = Color(120, 250, 154)
+# color9 = Color(250, 105, 224)
+# color10 = Color(44, 181, 233)
 
+# colorList = [color1, color2, color3, color4, color5, color6, color7, color8, color9, color10]
 
-# print(f'Simple RGB Euclidean: {simpleEuclideanRGBDistance(color1, color2)}')
-# print(f'Weighted RGB Euclidean: {weightedEuclideanRGBDistance(color1, color2)}')
-# print(f'Readmean RGB Euclidean: {redmeanEuclideanRGBDistance(color1, color2)}')
-# print (f'Taxicab L*ab: {taxicabLABDistance(color1, color2)}')
-# print(f'Delta E \'76 L*ab: {deltaE76LABDistance(color1, color2)}')
-
-
-# if(len(sys.argv) < 2):
-#     print("Must input video name")
-#     exit()
-# elif(len(sys.argv) == 2):
-#     print("Using default pixels of 1200 and 3000")
-#     pixelList = [1200, 3000]
-#     videoName = sys.argv[1]
-# else:
-#     videoName = sys.argv[1]
-#     pixelList = []
-#     for pixel in sys.argv[2:]:
-#         pixelList.append(int(pixel))
-
-# largestPixelIndex = largest(pixelList)
-
-# for pixel in pixelList:
-#     os.system(f'./main.run {pixel} {defaultMinPixels} {videoName}')
-#     pixelStr = str(pixel)
-#     os.system(f'mv output output{pixelStr}_{videoName[:-4]}')
-#     centroidListOutput = createCentroidList(f'output{pixelStr}_{videoName[:-4]}')
+# for color in colorList:
+#     allDifferences(refColor, color)
     
-#     centroidListList.append(centroidListOutput[0])
-#     centroidListExecution.append(centroidListOutput[2])
 
-# for x in range(len(centroidListList)):
-#     if(x != largestPixelIndex):
-#         compareOutputs(centroidListList[x], centroidListList[largestPixelIndex], centroidListExecution[x], centroidListExecution[largestPixelIndex], pixelList[x], pixelList[largestPixelIndex] )
+# print(f'Color 1 CIE-L*ab: {color1.lab.l}, {color1.lab.a}, {color1.lab.b}')
+# print(f'Color 2 CIE-L*ab: {color2.lab.l}, {color2.lab.a}, {color2.lab.b}')
+
+
+if(len(sys.argv) < 2):
+    print("Must input video name")
+    exit()
+elif(len(sys.argv) == 2):
+    print("Using default pixels of 1200 and 3000")
+    pixelList = [1200, 3000]
+    videoName = sys.argv[1]
+else:
+    videoName = sys.argv[1]
+    pixelList = []
+    for pixel in sys.argv[2:]:
+        pixelList.append(int(pixel))
+
+largestPixelIndex = largest(pixelList)
+
+for pixel in pixelList:
+    os.system(f'./main.run {pixel} {defaultMinPixels} {videoName}')
+    pixelStr = str(pixel)
+    os.system(f'mv output output{pixelStr}_{videoName[:-4]}')
+    centroidListOutput = createCentroidList(f'output{pixelStr}_{videoName[:-4]}')
+
+    centroidListList.append(centroidListOutput[0])
+    centroidListExecution.append(centroidListOutput[2])
+
+for x in range(len(centroidListList)):
+    if(x != largestPixelIndex):
+        compareOutputs(centroidListList[x], centroidListList[largestPixelIndex], centroidListExecution[x], centroidListExecution[largestPixelIndex], pixelList[x], pixelList[largestPixelIndex] )
 
 # script video [pixels pixels pixels...]
