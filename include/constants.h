8780465aa4b187050e7110754bd23538f6f02646
Tess Garrison <tessag159@icloud.com>
2021-03-23 21:38:15 -0500

    Changed path to source code

--- include/constants.h
+++ include/constants.h
@@ -6,7 +6,7 @@
 constexpr int MAX_CENTROIDS{6};
 constexpr int TARGET_PIXELS{1800};
 constexpr int MIN_PIXELS{800};
-const std::string PROJECT_PATH {"/home/tess/Code/KMeans_algorithm/"};
+const std::string PROJECT_PATH {"/home/tess/Code/VHS/"};
 
 // These are the CIE tristimulus x, y, and z reference values with a D65
 // observer illuminant at 2°
